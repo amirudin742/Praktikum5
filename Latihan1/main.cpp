e8231d54158899b1450424ad0b643e740a24098f
Amirudin742 <amirudin742@gmail.com>
2018-11-10 13:18:12 +0700

    Susunan Algoritma

---
+++ Latihan1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,max=0;
+    do{
+    cout << "Masukan Bilangan : ";
+    cin >> a;
+    if (a>max)
+        max=a;
+    }
+    while (a!=0);
+    cout <<max;
+    return 0;
+}
